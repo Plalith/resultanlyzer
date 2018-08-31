c4d1db978d0b65c71313f86f1d3fddbe158eefb5
Lalith Kumar <lalith889@gmail.com>
2018-08-31 05:33:22 +0530

    resultanalyze

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { CommonService } from '../common.service'
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +9,7 @@ import { CommonService } from '../common.service'
 })
 export class LoginComponent implements OnInit, DoCheck{
 
-  constructor(private commomservice:CommonService) { }
+  constructor(private commomservice:CommonService, private router:Router) { }
 
   ngOnInit() {
   }
@@ -17,4 +19,11 @@ export class LoginComponent implements OnInit, DoCheck{
   }
   logintype=this.commomservice.logintype;
 
+  login(f:NgForm){
+    if(this.logintype=='Student Login') {
+      this.router.navigateByUrl("/studentdashboard/sdashboard");
+    } else if(this.logintype=='College Area') {
+      this.router.navigateByUrl("/collegedashboard/cdashboard");
+    }
+  }
 }
